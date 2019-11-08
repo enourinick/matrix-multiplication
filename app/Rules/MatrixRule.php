f15eda79b4848748f97c516434c148bcafcb43bd
Mohammad Nourinik <enourinick@gmail.com>
2019-11-09 00:48:44 +0200

    Add a rule to determine if the given array is a valid matrix.

---
+++ app/Rules/MatrixRule.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class MatrixRule implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $columnCount = null;
+
+        foreach ($value as $row) {
+            if ($columnCount === null) {
+                $columnCount = count($row);
+            } else {
+                if($columnCount !== count($row)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute field is not a matrix';
+    }
+}
