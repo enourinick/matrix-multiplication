022152940dddfae77a41370dddb4a1993da4afe0
Mohammad Nourinik <enourinick@gmail.com>
2019-11-10 05:44:54 +0200

    Add HasApiTokens to the User model.

--- app/User.php
+++ app/User.php
@@ -2,13 +2,27 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
+/**
+ * User Model
+ *
+ * @package App
+ * @property integer id
+ * @property string name
+ * @property string email
+ * @property Carbon email_verified_at
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ * @property string password
+ */
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
