832379d55c2987ea360ac71f96f1759e367bf62d
Mohammad Nourinik <enourinick@gmail.com>
2019-11-10 17:11:22 +0200

    Add Basic preparation.

--- docker/core/start.sh
+++ docker/core/start.sh
@@ -12,4 +12,22 @@ service nginx start
 
 php-fpm&
 
+${INSTALL_DIR:=/var/www}
+
+# Check composer
+if [[ ! -d ${INSTALL_DIR}/vendor ]]; then
+  # No vendor folder, run composer install
+  composer install --no-scripts --no-progress --no-suggest -d ${INSTALL_DIR}
+fi
+
+# Check the database connection
+while php ${INSTALL_DIR}/artisan migrate:status | grep -q 'Connection refused'
+do
+ echo 'Waiting for database container getting up'
+ sleep 1s
+done
+
+# Run the log migration
+php ${INSTALL_DIR}/artisan migrate
+
 tail -f /var/log/messages
