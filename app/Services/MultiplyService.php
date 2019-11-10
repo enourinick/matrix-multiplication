6c38644442e12d1e0d1d7ee99c024d2b51e15be9
Mohammad Nourinik <enourinick@gmail.com>
2019-11-10 05:42:38 +0200

    Create MultiplyService.

---
+++ app/Services/MultiplyService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+class MultiplyService
+{
+    public function multiply(array $matrix1, array $matrix2)
+    {
+        $result = [];
+
+        for ($i = 0; $i < count($matrix1); $i++) {
+            for ($j = 0; $j < count($matrix2[0]); $j++) {
+                $result[$i][$j] = 0;
+                for ($k = 0; $k < count($matrix2); $k++) {
+                    $result[$i][$j] += $matrix1[$i][$k] * $matrix2[$k][$j];
+                }
+            }
+        }
+
+        return $result;
+    }
+}
