c0c252414462fd4be61b4a306cbaa8f776683772
Mohammad Nourinik <enourinick@gmail.com>
2019-11-10 05:52:29 +0200

    Code reformat.

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -6,13 +6,14 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 
-class CORS {
+class CORS
+{
 
     /**
      * Handle an incoming request.
      *
-     * @param  Request  $request
-     * @param  Closure  $next
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
@@ -20,17 +21,18 @@ class CORS {
         // ALLOW OPTIONS METHOD
         $headers = [
             "Access-Control-Allow-Origin" => '*',
-            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin, Accept, Authorization'
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Accept, Authorization'
         ];
-        if($request->getMethod() == "OPTIONS") {
+        if ($request->getMethod() == "OPTIONS") {
             // The client-side application can set only headers allowed in Access-Control-Allow-Headers
             return Response::make('OK', 200, $headers);
         }
 
         $response = $next($request);
-        foreach($headers as $key => $value)
+        foreach ($headers as $key => $value) {
             $response->header($key, $value);
+        }
         return $response;
     }
 }
